1d838a373b9000a0476eb2644bcbce427b4becb5
anhnhu <anhnt@subwallet.app>
2024-04-21 13:27:53 +0700

    Update change slug in game-item

--- src/api/game-item/services/game-item.ts
+++ src/api/game-item/services/game-item.ts
@@ -17,6 +17,8 @@ export default factories.createCoreService('api::game-item.game-item', ({strapi}
       // @ts-ignore
       d.gameId = d.game?.id || d.game;
       d.game && delete d.game;
+      // @ts-ignore
+      d.slug = null;
       if (d.itemGroup > 0 && d.itemGroupLevel > 0) {
         // @ts-ignore
         d.slug = `level${d.itemGroupLevel}`;
