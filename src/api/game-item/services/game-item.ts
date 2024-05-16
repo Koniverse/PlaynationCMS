62543f2017f48a0226db6312381ec45c420f9d16
khanhpv195 <khanhpoly@gmail.com>
2024-05-16 02:10:14 +0000

    khanhpv - add icon to gameitem

--- src/api/game-item/services/game-item.ts
+++ src/api/game-item/services/game-item.ts
@@ -3,19 +3,23 @@
  */
 
 import { factories } from '@strapi/strapi';
+import {GetValues} from "@strapi/types/dist/types/core/attributes";
 
 export default factories.createCoreService('api::game-item.game-item', ({strapi}) => ({
   async customList(params = {}) {
-    const data = await strapi.entityService.findMany('api::game-item.game-item', {
+    const data :GetValues<'api::game-item.game-item'>[]  = await strapi.entityService.findMany('api::game-item.game-item', {
       sort: 'id:asc',
       populate: {
+        'icon':true,
         'game': true,
       },
       ...params
     });
-    data.forEach((d) => {
+    console.log(data)
+    data.forEach((d :GetValues<'api::game-item.game-item'>) => {
       // @ts-ignore
       d.gameId = d.game?.id || d.game;
+      d.icon = d.icon?.url || d.icon;
       d.game && delete d.game;
       d.createdAt !== undefined && delete d.createdAt;
       d.updatedAt !== undefined && delete d.updatedAt;
