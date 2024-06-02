49c69f47105193bace0f452833d70801797b4030
anhnhu <anhnt@subwallet.app>
2024-06-02 22:39:47 +0700

    Add confirm send notification

--- src/plugins/github-action-trigger/admin/src/components/TelegramSendAction/index.tsx
+++ src/plugins/github-action-trigger/admin/src/components/TelegramSendAction/index.tsx
@@ -3,8 +3,36 @@ import {Button} from "@strapi/design-system"
 import {useSelector} from 'react-redux';
 import {useCMEditViewDataManager, useFetchClient, useNotification} from '@strapi/helper-plugin';
 import {TriggerButtonInfo} from "../../../../types";
+import {Box, Divider, Flex, Typography} from '@strapi/design-system';
+import {Dialog, DialogBody, DialogFooter} from '@strapi/design-system';
 
-console.log('co vao day ko ne')
+const formatDateFully = (date: Date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Tháng bắt đầu từ 0
+    const day = String(date.getDate()).padStart(2, '0');
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+
+    return `${hour}:${minute} ${day}/${month}/${year}`;
+};
+
+interface KeyValuePairProps {
+    label: string;
+    value?: string;
+}
+
+const KeyValuePair = ({label, value = '-'}: KeyValuePairProps) => {
+    return (
+        <Flex justifyContent="space-between">
+            <Typography as="dt" fontWeight="bold" textColor="neutral800" variant="pi">
+                {label}
+            </Typography>
+            <Typography as="dd" variant="pi" textColor="neutral600">
+                {value}
+            </Typography>
+        </Flex>
+    );
+};
 const Index = ({}) => {
     const toggleNotification = useNotification();
     const {modifiedData} = useCMEditViewDataManager();
@@ -15,7 +43,10 @@ const Index = ({}) => {
         return state['content-manager_editViewLayoutManager'] || {};
     });
     const [loading, setLoading] = useState(false);
-    const [triggerButtons, setTriggerButtons] = useState<TriggerButtonInfo[]>([])
+    const [lastTime, setLastTime] = useState<string>();
+    const [triggerButtons, setTriggerButtons] = useState<TriggerButtonInfo[]>([]);
+    const [isVisible, setIsVisible] = useState(false);
+    const [buttonActionId, setButtonActionId] = useState('');
 
     const showNotification = (message: string, type = 'warning') => {
         toggleNotification({
@@ -39,7 +70,10 @@ const Index = ({}) => {
                     params: {apiID},
                     validateStatus: (status: number) => status === 200,
                 });
-                const {enabled, buttons} = response.data;
+                const {enabled, buttons, lastTriggered} = response.data;
+                if (lastTriggered){
+                    setLastTime(formatDateFully(new Date(lastTriggered)));
+                }
 
                 setTriggerButtons(enabled ? buttons : [])
             } catch (e) {
@@ -49,32 +83,101 @@ const Index = ({}) => {
     }, [contentType?.apiID]);
 
     const handleClick = useCallback((buttonID: string) => async () => {
+            setButtonActionId(buttonID);
+            setIsVisible(true);
+        }
+        , []);
+
+    const handleSendAction = useCallback( async () =>  {
             try {
                 setLoading(true);
                 const id = modifiedData.id;
-                const response = await post(`github-action-trigger/telegram-trigger`, {buttonID, id}, {
+                const response = await post(`github-action-trigger/telegram-trigger`, {buttonID: buttonActionId, id}, {
                     headers: {
                         'Content-Type': 'application/json',
                     }
                 });
                 setLoading(false);
-                const {type, message} = response.data;
+                const {type, message, isProduction} = response.data;
                 showNotification(message, type);
+                setIsVisible(false);
+                if (isProduction === true) {
+                    setLastTime(formatDateFully(new Date()));
+                }
+                setButtonActionId('');
             } catch (e) {
                 setLoading(false);
             }
         }
-        , []);
+        , [buttonActionId]);
+    if (triggerButtons.length === 0) {
+        return <></>;
+    }
     return (
         <>
-            {triggerButtons.map(({buttonID, label, variant}) => (
-                <Button
-                    variant={variant || 'success-light'}
-                    key={buttonID} loading={loading}
-                    onClick={handleClick(buttonID)}>
-                    {label}
-                </Button>
-            ))}
+            <Dialog onClose={() => setIsVisible(false)} title="Confirmation" isOpen={isVisible}>
+                <DialogBody>
+                    <Flex direction="column" alignItems="center" gap={2}>
+                        <Flex justifyContent="center">
+                            <Typography id="confirm-description">Are you sure to send telegram notifications??</Typography>
+                        </Flex>
+                    </Flex>
+                </DialogBody>
+                <DialogFooter startAction={<Button onClick={() => setIsVisible(false)} variant="tertiary">
+                    Cancel
+                </Button>} endAction={<Button variant="danger-light" onClick={() =>  {
+                     console.log(
+                    'handleSendAction',
+                    modifiedData,
+                    buttonActionId
+                )
+                    handleSendAction().then(() => setIsVisible(false));
+                }}>
+                    Confirm
+                </Button>}/>
+            </Dialog>
+            <Box
+                as="aside"
+                aria-labelledby="additional-information"
+                background="neutral0"
+                borderColor="neutral150"
+                hasRadius
+                paddingBottom={4}
+                paddingLeft={4}
+                paddingRight={4}
+                paddingTop={6}
+                shadow="tableShadow"
+            >
+                <Flex direction="column" alignItems="stretch" gap={4}>
+                    <Flex direction="column" alignItems="stretch" gap={2}>
+                        <Typography variant="sigma" textColor="neutral600" id="additional-information">
+                            Telegram Action
+                        </Typography>
+
+                        <Box>
+                            <Divider/>
+                        </Box>
+                    </Flex>
+                    <Flex direction="column" alignItems="stretch" gap={4}>
+                        <Flex direction="column" alignItems="stretch" gap={2} as="dl">
+                            <KeyValuePair
+                                label={'Last build Production'}
+                                value={lastTime ?? 'No build yet'}
+                            />
+                        </Flex>
+                    </Flex>
+
+
+                    {triggerButtons.map(({buttonID, label, variant}) => (
+                        <Button
+                            variant={variant || 'success-light'}
+                            key={buttonID} loading={loading}
+                            onClick={handleClick(buttonID)}>
+                            {label}
+                        </Button>
+                    ))}
+                </Flex>
+            </Box>
         </>
     )
 }
--- src/plugins/github-action-trigger/server/services/telegramAction.ts
+++ src/plugins/github-action-trigger/server/services/telegramAction.ts
@@ -6,122 +6,135 @@ import {Strapi} from "@strapi/strapi";
 import {GithubActionEnabledResponse, TriggerButtonInfo} from "../../types";
 
 const getHeaders = (token: string) => ({
-  'Content-Type': 'application/json',
-  'authorization': `Bearer ${token}`,
+    'Content-Type': 'application/json',
+    'authorization': `Bearer ${token}`,
 })
 
-export default ({ strapi }: { strapi: Strapi }) => ({
-  async getRequireRoles(apiID: string) {
-    const ctx = strapi.requestContext.get();
-    const user = ctx?.state?.user;
-    const roleIds = user.roles.map((role) => role.id);
-    const singularName = `api::${apiID}.${apiID}`;
-    const permissionList = await strapi.entityService.findMany('admin::permission', {
-      filters: {
-        subject: singularName,
-        role: {
-          id: {
-            $in: roleIds
-          }}
-      },
-      populate: ['role'],
-    });
-    // @ts-ignore
-    return permissionList && permissionList.reduce((currentValue,permission) => {
-      const actionSplit = permission.action.split('.');
-      if (actionSplit.length > 0) {
-        currentValue.push(actionSplit[actionSplit.length - 1]);
-      }
-      return currentValue;
-    }, []);
-  },
-  async getButtons(apiID: string) {
-    // @ts-ignore
-    const actions = await strapi.admin.config.telegramActions;
-    const {triggerButtons} = actions;
-    let enabled = false;
-    const requireRoles = await this.getRequireRoles(apiID);
+export default ({strapi}: { strapi: Strapi }) => ({
+    async getRequireRoles(apiID: string) {
+        const ctx = strapi.requestContext.get();
+        const user = ctx?.state?.user;
+        const roleIds = user.roles.map((role) => role.id);
+        const singularName = `api::${apiID}.${apiID}`;
+        const permissionList = await strapi.entityService.findMany('admin::permission', {
+            filters: {
+                subject: singularName,
+                role: {
+                    id: {
+                        $in: roleIds
+                    }
+                }
+            },
+            populate: ['role'],
+        });
+        // @ts-ignore
+        return permissionList && permissionList.reduce((currentValue, permission) => {
+            const actionSplit = permission.action.split('.');
+            if (actionSplit.length > 0) {
+                currentValue.push(actionSplit[actionSplit.length - 1]);
+            }
+            return currentValue;
+        }, []);
+    },
+    async getButtons(apiID: string) {
+        // @ts-ignore
+        const actions = await strapi.admin.config.telegramActions;
+        const {triggerButtons} = actions;
+        let enabled = false;
+        const requireRoles = await this.getRequireRoles(apiID);
 
-    const buttons: TriggerButtonInfo[] = (triggerButtons || [])
-      .filter((button) => button.apiID === apiID && (!button.roles || button.roles.length === 0 || button.roles.every((role) => requireRoles.includes(role))))
-      .map(({
-              buttonID,
-              apiID,
-              label,
-              variant,
-        roles
-            }) => ({
-        buttonID,
-        apiID,
-        label,
-        variant,
-        roles
-      }));
-    if (buttons.length > 0) {
-      enabled = true;
-    }
 
-    return {
-      enabled,
-      buttons
-    } as GithubActionEnabledResponse;
-  },
-  async trigger(buttonID: string, id: number) {
-    // @ts-ignore
-    const actions = await strapi.admin.config.telegramActions;
-    const {triggerButtons, apiToken, apiActionUrl, apiActionDevelopmentUrl} = actions;
-    let hostUrl = apiActionUrl;
-    let executed = false;
-
-    const buttonInfo = triggerButtons.find((button) => button.buttonID === buttonID);
-    const {enabled} = await this.getButtons(buttonInfo.apiID);
-    if (!enabled) {
-      return {
-        executed,
-      }
-    }
-
-    if (buttonInfo) {
-      executed = true;
-      const {singularName, routerApi, environment} = buttonInfo;
-      if (environment === 'development') {
-        hostUrl = apiActionDevelopmentUrl;
-      }
-      const contentSend = await strapi.service(singularName).getDataContent(id);
-      const {status, message, data} = contentSend;
-      if (!status) {
-        return {
-          type: 'warning',
-          status: false,
-          message
+        const buttons: TriggerButtonInfo[] = (triggerButtons || [])
+            .filter((button) => button.apiID === apiID && (!button.roles || button.roles.length === 0 || button.roles.every((role) => requireRoles.includes(role))))
+            .map(({
+                      buttonID,
+                      apiID,
+                      label,
+                      variant,
+                      roles
+                  }) => ({
+                buttonID,
+                apiID,
+                label,
+                variant,
+                roles
+            }));
+        if (buttons.length > 0) {
+            enabled = true;
         }
-      }
-      try {
-        const url = `${hostUrl}${routerApi}`;
-        await axios.post(url, data, {headers: getHeaders(apiToken)});
+        const auditLogs = await strapi.entityService.findMany('api::audit-log.audit-log', {
+            filters: {
+                action: 'send_notification_production',
+            },
+            sort: 'createdAt:desc',
+            limit: 1
+        })
 
-        await strapi.services['api::audit-log.audit-log'].addAuditLogSendNotification(buttonInfo, id, environment);
         return {
-            type: 'success',
-            status: true,
-            message: 'Triggered successfully'
+            enabled,
+            buttons,
+            lastTriggered: auditLogs.length > 0 ? auditLogs[0].createdAt : null
+        } as GithubActionEnabledResponse;
+    },
+    async trigger(buttonID: string, id: number) {
+        // @ts-ignore
+        const actions = await strapi.admin.config.telegramActions;
+        const {triggerButtons, apiToken, apiActionUrl, apiActionDevelopmentUrl} = actions;
+        let hostUrl = apiActionUrl;
+        let executed = false;
+
+        const buttonInfo = triggerButtons.find((button) => button.buttonID === buttonID);
+        const {enabled} = await this.getButtons(buttonInfo.apiID);
+        if (!enabled) {
+            return {
+                executed,
+            }
         }
-      } catch (error) {
-        console.error('Error on trigger', error);
-        const data = error.response?.data;
-        if (data && data.message) {
-          return {
+
+        if (buttonInfo) {
+            executed = true;
+            let isProduction = true;
+            const {singularName, routerApi, environment} = buttonInfo;
+            if (environment === 'development') {
+                hostUrl = apiActionDevelopmentUrl;
+                isProduction = false;
+            }
+            const contentSend = await strapi.service(singularName).getDataContent(id);
+            const {status, message, data} = contentSend;
+            if (!status) {
+                return {
+                    type: 'warning',
+                    status: false,
+                    message
+                }
+            }
+            try {
+                const url = `${hostUrl}${routerApi}`;
+                await axios.post(url, data, {headers: getHeaders(apiToken)});
+
+                await strapi.services['api::audit-log.audit-log'].addAuditLogSendNotification(buttonInfo, id, environment);
+                return {
+                    type: 'success',
+                    status: true,
+                    isProduction,
+                    message: 'Triggered successfully'
+                }
+            } catch (error) {
+                console.error('Error on trigger', error);
+                const data = error.response?.data;
+                if (data && data.message) {
+                    return {
+                        type: 'warning',
+                        status: false,
+                        message: data.message
+                    }
+                }
+            }
+        }
+        return {
             type: 'warning',
             status: false,
-            message: data.message
-          }
+            message: 'Not Found'
         }
-      }
     }
-    return {
-      type: 'warning',
-      status: false,
-      message: 'Not Found'
-    }
-  }
 });
